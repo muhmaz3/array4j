5e5f15646d19a13791e6a24a20935c8d22d481fd
fullung <fullung@91ab28ee-cd28-0410-b00c-5fca7c34108d>
2007-07-21 22:40:00 +0000

    Fixed FloatMatrixTest for native matrix.

--- src/main/native/array4j.c
+++ src/main/native/array4j.c
@@ -1,9 +1,9 @@
-#include "com_googlecode_array4j_blas_DirectFloatBLAS_KernelImpl.h"
+#include "com_googlecode_array4j_FloatBLAS.h"
 
 #include <mkl.h>
 
-JNIEXPORT jfloat JNICALL Java_com_googlecode_array4j_blas_DirectFloatBLAS_00024KernelImpl_sdot
-  (JNIEnv *env, jobject self, jint n, jobject x, jint incx, jobject y, jint incy)
+JNIEXPORT jfloat JNICALL Java_com_googlecode_array4j_FloatBLAS_sdot
+  (JNIEnv *env, jclass clazz, jint n, jobject x, jint offx, jint incx, jobject y, jint offy, jint incy)
 {
     jfloat* xp = (jfloat*) (*env)->GetDirectBufferAddress(env, x);
     jfloat* yp = (jfloat*) (*env)->GetDirectBufferAddress(env, y);
@@ -13,5 +13,19 @@ JNIEXPORT jfloat JNICALL Java_com_googlecode_array4j_blas_DirectFloatBLAS_00024K
     if (xp == NULL) {
         (*env)->FatalError(env, "invalid buffer y");
     }
-    return cblas_sdot(n, xp, incx, yp, incy);
+    return cblas_sdot(n, &xp[offx], incx, &yp[offy], incy);
+}
+
+JNIEXPORT void JNICALL Java_com_googlecode_array4j_FloatBLAS_scopy
+  (JNIEnv *env, jclass clazz, jint n, jobject x, jint offx, jint incx, jobject y, jint offy, jint incy)
+{
+    jfloat* xp = (jfloat*) (*env)->GetDirectBufferAddress(env, x);
+    jfloat* yp = (jfloat*) (*env)->GetDirectBufferAddress(env, y);
+    if (xp == NULL) {
+        (*env)->FatalError(env, "invalid buffer x");
+    }
+    if (xp == NULL) {
+        (*env)->FatalError(env, "invalid buffer y");
+    }
+    cblas_scopy(n, &xp[offx], incx, &yp[offy], incy);
 }
