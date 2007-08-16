922095b05c23ae0d75c135ceeb5b3f54a84fd170
fullung <fullung@91ab28ee-cd28-0410-b00c-5fca7c34108d>
2007-08-16 07:10:02 +0000

    Added test for writeColumnsAsMatrix.

--- trunk/src/test/java/com/googlecode/array4j/io/MatrixIOStreamTest.java
+++ trunk/src/test/java/com/googlecode/array4j/io/MatrixIOStreamTest.java
@@ -5,15 +5,18 @@ import static org.junit.Assert.assertEquals;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.junit.Test;
 
 import com.googlecode.array4j.dense.FloatDenseMatrix;
+import com.googlecode.array4j.dense.FloatDenseVector;
 
 public final class MatrixIOStreamTest {
     @Test
-    public void test() throws IOException {
+    public void testWriteMatrix() throws IOException {
         FloatDenseMatrix matrix = new FloatDenseMatrix(10, 20);
         matrix.fillRandom(new Random(0));
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -23,6 +26,29 @@ public final class MatrixIOStreamTest {
         ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
         MatrixInputStream in = new MatrixInputStream(bais);
         FloatDenseMatrix matrix2 = in.readMatrix();
+        assertEquals(matrix.rows(), matrix2.rows());
+        assertEquals(matrix.columns(), matrix2.columns());
+        assertEquals(matrix, matrix2);
+    }
+
+    @Test
+    public void testColumnsAsMatrix() throws IOException {
+        FloatDenseMatrix matrix = new FloatDenseMatrix(3, 4);
+        matrix.fillRandom(new Random(0));
+        List<FloatDenseVector> columns = new ArrayList<FloatDenseVector>(matrix.columns());
+        for (FloatDenseVector column : matrix.columnsIterator()) {
+            columns.add(column);
+        }
+        assertEquals(matrix.columns(), columns.size());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        MatrixOutputStream out = new MatrixOutputStream(baos);
+        out.writeColumnsAsMatrix(columns);
+        out.close();
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        MatrixInputStream in = new MatrixInputStream(bais);
+        FloatDenseMatrix matrix2 = in.readMatrix();
+        assertEquals(matrix.rows(), matrix2.rows());
+        assertEquals(matrix.columns(), matrix2.columns());
         assertEquals(matrix, matrix2);
     }
 }
