868fe393f87a388f4b2e1417521369059ae2ed29
fullung <fullung@91ab28ee-cd28-0410-b00c-5fca7c34108d>
2007-08-27 09:07:44 +0000

    Fixed GEMM test.

--- trunk/src/test/java/com/googlecode/array4j/packed/FloatPackedMatrixTest.java
+++ trunk/src/test/java/com/googlecode/array4j/packed/FloatPackedMatrixTest.java
@@ -5,6 +5,6 @@ import org.junit.Test;
 public final class FloatPackedMatrixTest {
     @Test
     public void test() {
-        FloatPackedMatrix matrix = new FloatPackedMatrix(2, 2);
+        FloatPackedMatrix symm = FloatPackedMatrix.createSymmetric(2, 2);
     }
 }
