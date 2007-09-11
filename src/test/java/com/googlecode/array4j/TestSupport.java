9b5d624709b8d7013458ea10e599350637eda275
fullung <fullung@91ab28ee-cd28-0410-b00c-5fca7c34108d>
2007-09-11 19:25:55 +0000

    Fixed some issues with offset calculations for 0-length arrays.

---
+++ src/test/java/com/googlecode/array4j/TestSupport.java
@@ -0,0 +1,14 @@
+package com.googlecode.array4j;
+
+public final class TestSupport {
+    private TestSupport() {
+    }
+
+    public static void populateMatrix(final FloatMatrix<?, ?> x) {
+        for (int i = 0, k = 1; i < x.rows(); i++) {
+            for (int j = 0; j < x.columns(); j++, k++) {
+                x.set(i, j, k);
+            }
+        }
+    }
+}
--- src/test/java/com/googlecode/array4j/dense/FloatDenseUtilsTest.java
+++ src/test/java/com/googlecode/array4j/dense/FloatDenseUtilsTest.java
@@ -3,6 +3,7 @@ package com.googlecode.array4j.dense;
 import static org.junit.Assert.assertEquals;
 import com.googlecode.array4j.Orientation;
 import com.googlecode.array4j.Storage;
+import com.googlecode.array4j.TestSupport;
 import java.util.Arrays;
 import java.util.Collection;
 import org.junit.Test;
@@ -14,8 +15,7 @@ import org.junit.runners.Parameterized.Parameters;
 public final class FloatDenseUtilsTest {
     @Parameters
     public static Collection<?> data() {
-//        return Arrays.asList(new Object[][]{{Orientation.ROW}, {Orientation.COLUMN}});
-        return Arrays.asList(new Object[][]{{Orientation.ROW}});
+        return Arrays.asList(new Object[][]{{Orientation.ROW}, {Orientation.COLUMN}});
     }
 
     private final Orientation orientation;
@@ -29,15 +29,19 @@ public final class FloatDenseUtilsTest {
         for (int rows = 0; rows < 5; rows++) {
             for (int columns = 0; columns < 8; columns++) {
                 FloatDenseMatrix x = new FloatDenseMatrix(rows, columns, orientation, Storage.HEAP);
-                for (int j = 0; j < columns; j++) {
-                    FloatDenseMatrix y = FloatDenseUtils.subMatrixColumns(x, 0, j);
-                    assertEquals(x.rows(), y.rows());
-                    assertEquals(j, y.columns());
-                    for (int k = 0; k < j; k++) {
-                        for (int m = 0; m < x.rows(); m++) {
-                            assertEquals(x.get(m, k), y.get(m, k), 0);
+                TestSupport.populateMatrix(x);
+                for (int i = 0; i < columns; i++) {
+                    for (int j = i; j <= columns; j++) {
+                        FloatDenseMatrix y = FloatDenseUtils.subMatrixColumns(x, i, j);
+                        assertEquals(x.rows(), y.rows());
+                        assertEquals(j - i, y.columns());
+                        for (int k = i; k < j; k++) {
+                            int n = k - i;
+                            for (int m = 0; m < x.rows(); m++) {
+                                assertEquals(x.get(m, k), y.get(m, n), 0);
+                            }
+                            assertEquals(x.column(k), y.column(n));
                         }
-                        assertEquals(x.column(k), y.column(k));
                     }
                 }
             }
