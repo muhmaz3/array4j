f037c62836c72c95b4be2dc5a8a5493fd0394d67
fullung <fullung@91ab28ee-cd28-0410-b00c-5fca7c34108d>
2007-10-19 11:32:10 +0000

    Some work on supporting ACML.

--- src/main/native/array4j.c
+++ src/main/native/array4j.c
@@ -1,5 +1,12 @@
-#include <mkl.h>
 #include <stdlib.h>
+
+#if defined(ARRAY4J_HAVE_MKL)
+#include <mkl.h>
+#elif defined(ARRAY4J_HAVE_ACML)
+#include <acml.h>
+#else
+#error BLAS/LAPACK includes required
+#endif
 
 #ifdef _WIN32
 #define ARRAY4J_EXPORT __declspec(dllexport)
@@ -35,32 +42,57 @@ ARRAY4J_EXPORT long array4j_mkl_DftiErrorClass(long i, long j)
 #endif
 
 ARRAY4J_EXPORT void array4j_sgemm
-  (int order, int transa, int transb, int m, int n, int k, float alpha, const float* a, int lda, const float* b, int ldb, float beta, float *c, int ldc)
+  (int order, int transa, int transb, int m, int n, int k, float alpha, float* a, int lda, float* b, int ldb, float beta, float *c, int ldc)
 {
+#if defined(ARRAY4J_HAVE_MKL)
     cblas_sgemm(order, transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
+#elif defined(ARRAY4J_HAVE_ACML)
+    sgemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
+#else
+#error sgemm function required
+#endif
 }
 
 ARRAY4J_EXPORT void array4j_ssyrk
-  (int order, int uplo, int trans, int n, int k, float alpha, const float* a, int lda, float beta, float *c, int ldc)
+  (int order, int uplo, int trans, int n, int k, float alpha, float* a, int lda, float beta, float* c, int ldc)
 {
+#if defined(ARRAY4J_HAVE_MKL)
     cblas_ssyrk(order, uplo, trans, n, k, alpha, a, lda, beta, c, ldc);
+#elif defined(ARRAY4J_HAVE_ACML)
+    ssyrk(uplo, trans, n, k, alpha, a, lda, beta, c, ldc);
+#else
+#error ssyrk function required
+#endif
 }
 
 ARRAY4J_EXPORT float array4j_sdot
-  (int n, const float* x, int incx, const float *y, int incy)
+  (int n, float* x, int incx, float* y, int incy)
 {
+#if defined(ARRAY4J_HAVE_MKL)
     return cblas_sdot(n, x, incx, y, incy);
+#elif defined(ARRAY4J_HAVE_ACML)
+    return sdot(n, x, incx, y, incy);
+#else
+#error sdot function required
+#endif
 }
 
 ARRAY4J_EXPORT void array4j_saxpy
-  (int n, float a, const float* x, int incx, float *y, int incy)
+  (int n, float a, float* x, int incx, float* y, int incy)
 {
+#if defined(ARRAY4J_HAVE_MKL)
     cblas_saxpy(n, a, x, incx, y, incy);
+#elif defined(ARRAY4J_HAVE_ACML)
+    saxpy(n, a, x, incx, y, incy);
+#else
+#error saxpy function required
+#endif
 }
 
 ARRAY4J_EXPORT void array4j_log
   (int n, const float* x, int incx, float* y, int incy)
 {
+#if defined(ARRAY4J_HAVE_MKL)
     float* a = incx == 1 ? x : malloc(n * sizeof(float));
     float* r = incy == 1 ? y : malloc(n * sizeof(float));
     if (incx != 1) {
@@ -74,4 +106,8 @@ ARRAY4J_EXPORT void array4j_log
         vsUnpackI(n, r, y, incy);
         free(r);
     }
+#elif defined(ARRAY4J_HAVE_ACML)
+#else
+#error log function required
+#endif
 }
