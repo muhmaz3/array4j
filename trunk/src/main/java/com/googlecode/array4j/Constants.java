3d0e4d9b0b1c3b1df256a938b35c180ef5284b44
fullung <fullung@91ab28ee-cd28-0410-b00c-5fca7c34108d>
2007-11-29 11:48:12 +0000

    Comment tweaks.

--- trunk/src/main/java/com/googlecode/array4j/Constants.java
+++ trunk/src/main/java/com/googlecode/array4j/Constants.java
@@ -1,10 +1,14 @@
 package com.googlecode.array4j;
 
 public final class Constants {
-    /** Size of a <CODE>double</CODE> in bytes. */
+    /**
+     * The number of bytes used to represent a <tt>double</tt> value.
+     */
     public static final int DOUBLE_BYTES = Double.SIZE >>> 3;
 
-    /** Size of a <CODE>float</CODE> in bytes. */
+    /**
+     * The number of bytes used to represent a <tt>float</tt> value.
+     */
     public static final int FLOAT_BYTES = Float.SIZE >>> 3;
 
     private Constants() {
