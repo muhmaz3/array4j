2ac6637041f92fc048758ec2362d957f08b0d625
fullung <fullung@91ab28ee-cd28-0410-b00c-5fca7c34108d>
2008-02-23 23:41:55 +0000

    Work in progress on HTK I/O.

---
+++ src/main/java/net/lunglet/htk/HTKFlags.java
@@ -0,0 +1,21 @@
+package net.lunglet.htk;
+
+public interface HTKFlags {
+    /** Has 0th cepstral coefficient. */
+    int C0 = 0x2000;
+
+    /** Has acceleration coefficients. */
+    int HAS_ACCELERATION = 0x200;
+
+    /** Has delta coefficients. */
+    int HAS_DELTA = 0x100;
+
+    /** Has energy. */
+    int HAS_ENERGY = 0x40;
+
+    /** Suppress absolute energy. */
+    int SUPPRESS_ABSOLUTE_ENERGY = 0x80;
+
+    /** Zero mean static coefficients. */
+    int ZERO_MEAN = 0x800;
+}
