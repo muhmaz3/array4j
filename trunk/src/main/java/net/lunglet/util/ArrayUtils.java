674d7e58aaae0d4b6c9b139ae7d390a34e89369f
fullung <fullung@91ab28ee-cd28-0410-b00c-5fca7c34108d>
2008-02-27 21:56:46 +0000

    Fixed Arrays#copyOf. Added assertNotNull.

--- trunk/src/main/java/net/lunglet/util/ArrayUtils.java
+++ trunk/src/main/java/net/lunglet/util/ArrayUtils.java
@@ -6,7 +6,9 @@ public final class ArrayUtils {
     public static float[][] copyOf(final float[][] x, final int newLength) {
         float[][] y = new float[newLength][];
         for (int i = 0; i < y.length; i++) {
-            y[i] = Arrays.copyOf(x[i], x[i].length);
+            if (x[i] != null) {
+                y[i] = Arrays.copyOf(x[i], x[i].length);
+            }
         }
         return y;
     }
--- trunk/src/test/java/net/lunglet/util/ArrayUtilsTest.java
+++ trunk/src/test/java/net/lunglet/util/ArrayUtilsTest.java
@@ -2,14 +2,20 @@ package net.lunglet.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import org.junit.Test;
 
 public final class ArrayUtilsTest {
     @Test
     public void testCopyOf() {
-        float[][] values = {{1.0f, 2.0f}, {3.0f, 4.0f, 5.0f}};
+        float[][] values = {{1.0f, 2.0f}, null, {3.0f, 4.0f, 5.0f}};
         float[][] values2 = ArrayUtils.copyOf(values, values.length);
         for (int i = 0; i < values.length; i++) {
+            // check that null row is handled properly
+            if (values[i] == null) {
+                assertNull(values2[i]);
+                continue;
+            }
             for (int j = 0; j < values[i].length; j++) {
                 float expected = values[i][j];
                 assertEquals(expected, values2[i][j], 0);
