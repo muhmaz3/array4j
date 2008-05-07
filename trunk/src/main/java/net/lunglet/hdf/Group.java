4415c7258509c1411703d670686c4cf5c67795dc
fullung <fullung@91ab28ee-cd28-0410-b00c-5fca7c34108d>
2008-05-07 22:30:03 +0000

    Try to fix some bugs.

--- trunk/src/main/java/net/lunglet/hdf/Group.java
+++ trunk/src/main/java/net/lunglet/hdf/Group.java
@@ -42,7 +42,7 @@ public final class Group extends H5Object {
         synchronized (H5Library.INSTANCE) {
             datasetId = H5Library.INSTANCE.H5Dcreate(getId(), name, typeId, spaceId, createPlistId);
             if (datasetId < 0) {
-                throw new H5GroupException("H5Dcreate failed", true);
+                throw new H5GroupException("H5Dcreate of " + name + " failed", true);
             }
         }
         return new DataSet(datasetId);
@@ -63,7 +63,7 @@ public final class Group extends H5Object {
         synchronized (H5Library.INSTANCE) {
             groupId = H5Library.INSTANCE.H5Gcreate(getId(), name, new NativeLong(0));
             if (groupId < 0) {
-                throw new H5GroupException("H5Gcreate failed");
+                throw new H5GroupException("H5Gcreate of " + name + " failed");
             }
         }
         return new Group(groupId, false);
